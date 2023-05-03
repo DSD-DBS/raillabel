ad326bf79f2e3bc16aa981952ceed72f2e43211f
Tobias Klockau <tobias.klockau@deutschebahn.com>
2023-05-03 14:33:45 +0200

    feat: load uai coordinate system

--- raillabel/_understand_ai_t4_format/__init__.py
+++ raillabel/_understand_ai_t4_format/__init__.py
@@ -1,5 +1,6 @@
 # Copyright DB Netz AG and contributors
 # SPDX-License-Identifier: Apache-2.0
 
+from .coordinate_system import CoordinateSystem
 from .metadata import Metadata
 from .scene import Scene
