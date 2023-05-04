0e74337cfeca2d932e59c0b31b54c001a6ccc60d
Tobias Klockau <tobias.klockau@deutschebahn.com>
2023-05-04 09:17:22 +0200

    Revert "feat: load uai coordinate system"
    This reverts commit 41b80f1b52e95e8cd6751cccab2314cc50164894.

--- raillabel/_understand_ai_t4_format/__init__.py
+++ raillabel/_understand_ai_t4_format/__init__.py
@@ -1,6 +1,5 @@
 # Copyright DB Netz AG and contributors
 # SPDX-License-Identifier: Apache-2.0
 
-from .coordinate_system import CoordinateSystem
 from .metadata import Metadata
 from .scene import Scene
