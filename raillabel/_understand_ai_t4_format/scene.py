8381c99f54a2d3931d7bad7bb216ea7ca8ff87c8
Tobias Klockau <tobias.klockau@deutschebahn.com>
2023-05-04 09:22:43 +0200

    Revert "fix: lint errors again"
    This reverts commit 40c8c367951fd722234c5c115d2971237bf7d455.

--- raillabel/_understand_ai_t4_format/scene.py
+++ raillabel/_understand_ai_t4_format/scene.py
@@ -1,9 +1,12 @@
 # Copyright DB Netz AG and contributors
 # SPDX-License-Identifier: Apache-2.0
 
+import typing as t
 from dataclasses import dataclass
 
 
 @dataclass
 class Scene:
     """The root Understand.Ai class, which contains all data."""
+
+    pass
