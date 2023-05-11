474e299b5ac77c41925b27a0a58a635ac1037b84
Tobias Klockau <tobias.klockau@deutschebahn.com>
2023-05-11 09:55:35 +0200

    feat: load uai point 3d

--- raillabel/_understand_ai_t4_format/__init__.py
+++ raillabel/_understand_ai_t4_format/__init__.py
@@ -4,6 +4,7 @@
 from .bounding_box_2d import BoundingBox2d
 from .coordinate_system import CoordinateSystem
 from .metadata import Metadata
+from .point_3d import Point3d
 from .scene import Scene
 from .sensor_reference import SensorReference
 from .size_3d import Size3d
