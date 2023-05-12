d25a2d9ecd04b4a744fcf7458f1f37f53839464e
Tobias Klockau <tobias.klockau@deutschebahn.com>
2023-05-12 12:30:40 +0200

    test: load uai coordinate system lidar

--- tests/test_raillabel/_understand_ai_t4_format/test_coordinate_system.py
+++ tests/test_raillabel/_understand_ai_t4_format/test_coordinate_system.py
@@ -28,6 +28,22 @@ def test_fromdict_camera(json_data):
     assert coordinate_system.camera_matrix == input_data["camera_matrix"]
     assert coordinate_system.dist_coeffs == input_data["dist_coeffs"]
 
+def test_fromdict_lidar(json_data):
+    input_data = json_data["_understand_ai_t4_format/coordinate_system_lidar"]
+    coordinate_system = uai_format.CoordinateSystem.fromdict(input_data)
+
+    assert coordinate_system.uid == input_data["coordinate_system_id"]
+    assert coordinate_system.topic == input_data["topic"]
+    assert coordinate_system.frame_id == input_data["frame_id"]
+    assert coordinate_system.position == input_data["position"]
+    assert coordinate_system.rotation_quaternion == input_data["rotation_quaternion"]
+    assert coordinate_system.rotation_matrix == input_data["rotation_matrix"]
+    assert coordinate_system.angle_axis_rotation == input_data["angle_axis_rotation"]
+    assert coordinate_system.homogeneous_transform == input_data["homogeneous_transform"]
+    assert coordinate_system.measured_position == None
+    assert coordinate_system.camera_matrix == None
+    assert coordinate_system.dist_coeffs == None
+
 
 # Executes the test if the file is called
 if __name__ == "__main__":
