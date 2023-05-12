d3f0cbf7dd965c2b96952eeac0dc112cbffbb589
Tobias Klockau <tobias.klockau@deutschebahn.com>
2023-05-12 12:47:41 +0200

    test: abstracted input for polyline 2d

--- tests/test_raillabel/_understand_ai_t4_format/test_polyline_2d.py
+++ tests/test_raillabel/_understand_ai_t4_format/test_polyline_2d.py
@@ -13,42 +13,9 @@ sys.path.insert(1, str(Path(__file__).parent.parent.parent.parent))
 import raillabel._understand_ai_t4_format as uai_format
 
 
-def test_fromdict():
-    input_data = {
-        "id": "7f2b99b7-61e4-4f9f-96e9-d3e9f583d7c2",
-        "objectId": "4d8eca35-6c1d-4159-8062-21c2f2c051df",
-        "className": "ir_track",
-        "geometry": {
-            "points": [
-                [
-                    493.20616179847406,
-                    479.9728191806302
-                ],
-                [
-                    542.6546426503735,
-                    431.74800354374366
-                ],
-                [
-                    589.9350150621058,
-                    385.51547643522207
-                ],
-                [
-                    639.9702598289472,
-                    335.8846553104306
-                ]
-            ]
-        },
-        "attributes": {
-            "trackId": "left_first_track",
-            "railSide": "rightRail",
-            "occlusion": "0-25%"
-        },
-        "sensor": {
-            "type": "ir_left",
-            "uri": "A0001780_image/000_1632321843.100464380.png",
-            "timestamp": "1632321843.100464380"
-        }
-    }
+def test_fromdict(json_data):
+    input_data = json_data["_understand_ai_t4_format/polyline_2d"]
+    input_data["sensor"] = json_data["_understand_ai_t4_format/sensor_reference_camera"]
     polyline = uai_format.Polyline2d.fromdict(input_data)
 
     assert polyline.id == UUID(input_data["id"])
