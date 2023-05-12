075478ae0e98fb8c13d448a7fb4e8633e60f3fff
Tobias Klockau <tobias.klockau@deutschebahn.com>
2023-05-12 11:04:49 +0200

    test: abstracted input data for sensor reference

--- tests/test_raillabel/_understand_ai_t4_format/test_sensor_reference.py
+++ tests/test_raillabel/_understand_ai_t4_format/test_sensor_reference.py
@@ -13,16 +13,13 @@ sys.path.insert(1, str(Path(__file__).parent.parent.parent.parent))
 import raillabel._understand_ai_t4_format as uai_format
 
 
-def test_fromdict():
-    sensor_reference = uai_format.SensorReference.fromdict({
-        "type": "test_id",
-        "uri": "test_folder/000_1632321843.100464380.png",
-        "timestamp": "1632321843.100464380"
-    })
+def test_fromdict(json_data):
+    input_data = json_data["_understand_ai_t4_format/sensor_reference_camera"]
+    sensor_reference = uai_format.SensorReference.fromdict(input_data)
 
-    assert sensor_reference.type == "test_id"
-    assert sensor_reference.uri == "test_folder/000_1632321843.100464380.png"
-    assert sensor_reference.timestamp == Decimal("1632321843.100464380")
+    assert sensor_reference.type == input_data["type"]
+    assert sensor_reference.uri == input_data["uri"]
+    assert sensor_reference.timestamp == Decimal(input_data["timestamp"])
 
 # Executes the test if the file is called
 if __name__ == "__main__":
