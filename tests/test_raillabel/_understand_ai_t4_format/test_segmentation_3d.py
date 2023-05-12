c4c5a51c691c3a17679f45f11ed8be591b75a995
Tobias Klockau <tobias.klockau@deutschebahn.com>
2023-05-12 12:53:30 +0200

    test: abstracted input for segmentation 3d

--- tests/test_raillabel/_understand_ai_t4_format/test_segmentation_3d.py
+++ tests/test_raillabel/_understand_ai_t4_format/test_segmentation_3d.py
@@ -13,30 +13,9 @@ sys.path.insert(1, str(Path(__file__).parent.parent.parent.parent))
 import raillabel._understand_ai_t4_format as uai_format
 
 
-def test_fromdict():
-    input_data = {
-        "id": "13478f94-d556-4f64-a72b-47662e94988e",
-        "objectId": "05a7e7a7-91e1-49ef-a172-780f2461f013",
-        "className": "3D_catenary_pole",
-        "geometry": {
-            "associatedPoints": [
-                39814,
-                39815,
-                39816,
-                39817,
-                39818
-            ],
-            "numberOfPointsInBox": 5
-        },
-        "attributes": {
-            "structure": "structured"
-        },
-        "sensor": {
-            "type": "LIDAR",
-            "uri": "lidar_merged/000_1632321880.132833000.pcd",
-            "timestamp": "1632321880.132833000"
-        }
-    }
+def test_fromdict(json_data):
+    input_data = json_data["_understand_ai_t4_format/segmentation_3d"]
+    input_data["sensor"] = json_data["_understand_ai_t4_format/sensor_reference_lidar"]
     segmentation_3d = uai_format.Segmentation3d.fromdict(input_data)
 
     assert segmentation_3d.id == UUID(input_data["id"])
