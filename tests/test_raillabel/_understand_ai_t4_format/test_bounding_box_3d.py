79bbcc986f77efa19c0a3a28a893012027f09ec7
Tobias Klockau <tobias.klockau@deutschebahn.com>
2023-05-12 11:15:32 +0200

    test: abstracted input for bounding box 3d

--- tests/test_raillabel/_understand_ai_t4_format/test_bounding_box_3d.py
+++ tests/test_raillabel/_understand_ai_t4_format/test_bounding_box_3d.py
@@ -13,41 +13,9 @@ sys.path.insert(1, str(Path(__file__).parent.parent.parent.parent))
 import raillabel._understand_ai_t4_format as uai_format
 
 
-def test_fromdict():
-    input_data = {
-        "id": "2f2a1d7f-56d1-435c-a3ec-d6b8fdaaa965",
-        "objectId": "48c988bd-76f1-423f-b46d-7e7acb859f31",
-        "className": "test_class",
-        "geometry": {
-            "size": {
-                "height": 12.887274595406309,
-                "width": 0.9726718154765793,
-                "length": 0.7487449536720978
-            },
-            "center": {
-                "x": 27.075067129430366,
-                "y": -3.7850908693727328,
-                "z": 6.671744016124283
-            },
-            "quaternion": {
-                "x": -7.710484478135359e-17,
-                "y": -7.978951661384013e-17,
-                "z": 0.7191010536652217,
-                "w": 0.6949055148849863
-            }
-        },
-        "attributes": {
-            "isDummy": False,
-            "carries": "nothing",
-            "connectedTo": [],
-            "pose": "upright",
-        },
-        "sensor": {
-            "type": "LIDAR",
-            "uri": "lidar_merged/000_1632321880.132833000.pcd",
-            "timestamp": "1632321880.132833000"
-        }
-    }
+def test_fromdict(json_data):
+    input_data = json_data["_understand_ai_t4_format/bounding_box_3d"]
+    input_data["sensor"] = json_data["_understand_ai_t4_format/sensor_reference_lidar"]
     bounding_box = uai_format.BoundingBox3d.fromdict(input_data)
 
     assert bounding_box.id == UUID(input_data["id"])
