9900c646d79659ca9109707688c12c5a36fa0cae
Tobias Klockau <tobias.klockau@deutschebahn.com>
2023-05-22 19:41:11 +0200

    feat: uai coordinate system to_raillabel() lidar

--- tests/test_raillabel/_understand_ai_t4_format/test_coordinate_system.py
+++ tests/test_raillabel/_understand_ai_t4_format/test_coordinate_system.py
@@ -54,6 +54,14 @@ def test_to_raillabel_camera(json_data):
     assert output_cs == ground_truth_cs
     assert output_stream == ground_truth_stream
 
+def test_to_raillabel_lidar(json_data):
+    input_data = json_data["_understand_ai_t4_format/coordinate_system_lidar"]
+    coordinate_system = uai_format.CoordinateSystem.fromdict(input_data)
+    _, output_stream = coordinate_system.to_raillabel()
+    ground_truth_stream = json_data["_understand_ai_t4_format/coordinate_system_lidar_stream_raillabel"]
+
+    assert output_stream == ground_truth_stream
+
 # Executes the test if the file is called
 if __name__ == "__main__":
     os.system("clear")
