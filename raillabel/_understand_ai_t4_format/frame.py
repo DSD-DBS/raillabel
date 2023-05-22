769fe703d4954c92033049b00c6e29daef706d0a
Tobias Klockau <tobias.klockau@deutschebahn.com>
2023-05-22 16:35:31 +0200

    fix: split up uai frame annotations by type

--- raillabel/_understand_ai_t4_format/frame.py
+++ raillabel/_understand_ai_t4_format/frame.py
@@ -5,6 +5,8 @@ import typing as t
 from dataclasses import dataclass
 from decimal import Decimal
 
+from ._annotation import _Annotation
+from ._translation import translate_sensor_id
 from .bounding_box_2d import BoundingBox2d
 from .bounding_box_3d import BoundingBox3d
 from .polygon_2d import Polygon2d
@@ -28,9 +30,11 @@ class Frame:
 
     id: int
     timestamp: Decimal
-    annotations: t.Dict[
-        str, t.Union[BoundingBox2d, BoundingBox3d, Polygon2d, Polyline2d, Segmentation3d]
-    ]
+    bounding_box_2ds: t.Dict[str, BoundingBox2d]
+    bounding_box_3ds: t.Dict[str, BoundingBox3d]
+    polygon_2ds: t.Dict[str, Polygon2d]
+    polyline_2ds: t.Dict[str, Polyline2d]
+    segmentation_3ds: t.Dict[str, Segmentation3d]
 
     @classmethod
     def fromdict(cls, data_dict: dict) -> "Frame":
@@ -50,28 +54,50 @@ class Frame:
         return Frame(
             id=int(data_dict["frameId"]),
             timestamp=Decimal(data_dict["timestamp"]),
-            annotations=cls._annotations_fromdict(data_dict["annotations"]),
+            bounding_box_2ds=cls._annotation_fromdict(
+                data_dict["annotations"]["2D_BOUNDING_BOX"], BoundingBox2d
+            ),
+            bounding_box_3ds=cls._annotation_fromdict(
+                data_dict["annotations"]["3D_BOUNDING_BOX"], BoundingBox3d
+            ),
+            polygon_2ds=cls._annotation_fromdict(data_dict["annotations"]["2D_POLYGON"], Polygon2d),
+            polyline_2ds=cls._annotation_fromdict(
+                data_dict["annotations"]["2D_POLYLINE"], Polyline2d
+            ),
+            segmentation_3ds=cls._annotation_fromdict(
+                data_dict["annotations"]["3D_SEGMENTATION"], Segmentation3d
+            ),
+        )
+
+    def to_raillabel(self) -> t.Tuple[dict, t.List[str], t.Dict[str, str]]:
+        """Generate a Frame from a dictionary in the UAI format.
+
+        Parameters
+        ----------
+        data_dict: dict
+            Understand.AI T4 format dictionary containing the data_dict.
+
+        Returns
+        -------
+        Frame
+            Converted frame.
+        list[str]
+            List of sensor ids contained in the frame.
+        dict[str, str]
+            Dictionary with one item per object in the frame. Keys are the object UUIDs, the
+            values are the class names.
+        """
+        return (
+            {
+                "frame_properties": self._frame_properties_to_raillabel(),
+                "objects": self._objects_to_raillabel(),
+            },
+            list(self._contained_sensors().keys()),
+            list(self._contained_objects().keys()),
         )
 
     @classmethod
-    def _annotations_fromdict(
-        cls, data_dict: dict
-    ) -> t.Dict[str, t.Union[BoundingBox2d, BoundingBox3d, Polygon2d, Polyline2d, Segmentation3d]]:
-        annotations = {}
-
-        for ann in data_dict["2D_BOUNDING_BOX"]:
-            annotations[ann["id"]] = BoundingBox2d.fromdict(ann)
-
-        for ann in data_dict["3D_BOUNDING_BOX"]:
-            annotations[ann["id"]] = BoundingBox3d.fromdict(ann)
-
-        for ann in data_dict["2D_POLYGON"]:
-            annotations[ann["id"]] = Polygon2d.fromdict(ann)
-
-        for ann in data_dict["2D_POLYLINE"]:
-            annotations[ann["id"]] = Polyline2d.fromdict(ann)
-
-        for ann in data_dict["3D_SEGMENTATION"]:
-            annotations[ann["id"]] = Segmentation3d.fromdict(ann)
-
-        return annotations
+    def _annotation_fromdict(
+        cls, data_dict: dict, annotation_class: t.Type[_Annotation]
+    ) -> t.Dict[str, BoundingBox2d]:
+        return {ann["id"]: annotation_class.fromdict(ann) for ann in data_dict}
--- tests/test_raillabel/_understand_ai_t4_format/test_frame.py
+++ tests/test_raillabel/_understand_ai_t4_format/test_frame.py
@@ -28,28 +28,45 @@ def _prepare_frame_data(frame_data: dict, json_data: dict) -> dict:
     return frame_data
 
 
+def _prepare_ground_truth(ground_truth: dict, json_data:dict) -> dict:
+    ground_truth["objects"]["48c988bd-76f1-423f-b46d-7e7acb859f31"]["object_data"]["bbox"] = [
+        json_data["_understand_ai_t4_format/bounding_box_2d_raillabel"]
+    ]
+    ground_truth["objects"]["48c988bd-76f1-423f-b46d-7e7acb859f31"]["object_data"]["cuboid"] = [
+        json_data["_understand_ai_t4_format/bounding_box_3d_raillabel"]
+    ]
+    ground_truth["objects"]["0f90cffa-2b6b-4e09-8fc2-527769a94e0a"]["object_data"]["poly2d"] = [
+        json_data["_understand_ai_t4_format/polygon_2d_raillabel"]
+    ]
+    ground_truth["objects"]["48c988bd-76f1-423f-b46d-7e7acb859f31"]["object_data"]["poly2d"] = [
+        json_data["_understand_ai_t4_format/polyline_2d_raillabel"]
+    ]
+
+    return ground_truth
+
+
 def test_fromdict(json_data):
     input_data = _prepare_frame_data(json_data["_understand_ai_t4_format/frame"], json_data)
     frame = uai_format.Frame.fromdict(input_data)
 
     assert frame.id == int(input_data["frameId"])
     assert frame.timestamp == Decimal(input_data["timestamp"])
-    assert len(frame.annotations) == 5
-    assert frame.annotations[json_data["_understand_ai_t4_format/bounding_box_2d"]["id"]] == uai_format.BoundingBox2d.fromdict(
+    assert frame.bounding_box_2ds[json_data["_understand_ai_t4_format/bounding_box_2d"]["id"]] == uai_format.BoundingBox2d.fromdict(
         json_data["_understand_ai_t4_format/bounding_box_2d"]
     )
-    assert frame.annotations[json_data["_understand_ai_t4_format/polyline_2d"]["id"]] == uai_format.Polyline2d.fromdict(
+    assert frame.polyline_2ds[json_data["_understand_ai_t4_format/polyline_2d"]["id"]] == uai_format.Polyline2d.fromdict(
         json_data["_understand_ai_t4_format/polyline_2d"]
     )
-    assert frame.annotations[json_data["_understand_ai_t4_format/polygon_2d"]["id"]] == uai_format.Polygon2d.fromdict(
+    assert frame.polygon_2ds[json_data["_understand_ai_t4_format/polygon_2d"]["id"]] == uai_format.Polygon2d.fromdict(
         json_data["_understand_ai_t4_format/polygon_2d"]
     )
-    assert frame.annotations[json_data["_understand_ai_t4_format/bounding_box_3d"]["id"]] == uai_format.BoundingBox3d.fromdict(
+    assert frame.bounding_box_3ds[json_data["_understand_ai_t4_format/bounding_box_3d"]["id"]] == uai_format.BoundingBox3d.fromdict(
         json_data["_understand_ai_t4_format/bounding_box_3d"]
     )
-    assert frame.annotations[json_data["_understand_ai_t4_format/segmentation_3d"]["id"]] == uai_format.Segmentation3d.fromdict(
+    assert frame.segmentation_3ds[json_data["_understand_ai_t4_format/segmentation_3d"]["id"]] == uai_format.Segmentation3d.fromdict(
         json_data["_understand_ai_t4_format/segmentation_3d"]
     )
+
 
 # Executes the test if the file is called
 if __name__ == "__main__":
