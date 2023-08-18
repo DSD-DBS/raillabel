03ad9995e88df3dd605d5868d9466dd3de259261
Tobias Klockau <tobias.klockau@deutschebahn.com>
2023-08-18 11:50:50 +0200

    feat: Size2d.fromdict() + unit tests

--- raillabel/format/size2d.py
+++ raillabel/format/size2d.py
@@ -18,3 +18,34 @@ class Size2d:
 
     x: float
     y: float
+
+    @classmethod
+    def fromdict(cls, data_dict: dict) -> "Size2d":
+        """Generate a Size2d object from a dict.
+
+        Parameters
+        ----------
+        data_dict: dict
+            RailLabel format snippet containing the relevant data.
+        """
+
+        return Size2d(
+            x=data_dict[0],
+            y=data_dict[1],
+        )
+
+    def asdict(self) -> dict:
+        """Export self as a dict compatible with the OpenLABEL schema.
+
+        Returns
+        -------
+        dict_repr: dict
+            Dict representation of this class instance.
+
+        Raises
+        ------
+        ValueError
+            if an attribute can not be converted to the type required by the OpenLabel schema.
+        """
+
+        return [float(self.x), float(self.y)]
