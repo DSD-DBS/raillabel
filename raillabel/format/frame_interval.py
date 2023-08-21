19e84c2d6b7d95bd8aa4a99116245030bf562701
Tobias Klockau <tobias.klockau@deutschebahn.com>
2023-08-21 09:45:09 +0200

    feat: FrameInterval.from_frame_uids

--- raillabel/format/frame_interval.py
+++ raillabel/format/frame_interval.py
@@ -1,7 +1,10 @@
 # Copyright DB Netz AG and contributors
 # SPDX-License-Identifier: Apache-2.0
 
+import typing as t
 from dataclasses import dataclass
+
+import numpy as np
 
 
 @dataclass
@@ -34,6 +37,39 @@ class FrameInterval:
             frame_end=data_dict["frame_end"],
         )
 
+    @classmethod
+    def from_frame_uids(cls, frame_uids: t.List[int]) -> t.List["FrameInterval"]:
+        """Convert a list of frame uids into FrameIntervals.
+
+        Parameters
+        ----------
+        frame_uids: list[int]
+            List of frame uids, that should be included in the FrameIntervals.
+
+        Returns
+        -------
+        list[FrameInterval]
+            FrameIntervals corresponding to the frames ids.
+
+        Example
+        -------
+        FrameInterval.from_frame_uids([0, 1, 2, 3, 6, 7, 9, 12, 13, 14]) == [
+            FrameInterval(0, 3),
+            FrameInterval(6, 7),
+            FrameInterval(9, 9),
+            FrameInterval(12, 14),
+        ]
+        """
+
+        sorted_frame_uids = sorted(frame_uids)
+        frame_uid_intervals = cls._slice_into_intervals(sorted_frame_uids)
+
+        frame_intervals = []
+        for interval in frame_uid_intervals:
+            frame_intervals.append(FrameInterval(frame_start=interval[0], frame_end=interval[-1]))
+
+        return frame_intervals
+
     def asdict(self) -> dict:
         """Export self as a dict compatible with the OpenLABEL schema.
 
@@ -56,3 +92,26 @@ class FrameInterval:
     def __len__(self) -> int:
         """Return the length in frames."""
         return abs(self.frame_start - self.frame_end) + 1
+
+    @classmethod
+    def _slice_into_intervals(cls, sorted_frame_uids: t.List[int]) -> t.List[t.List[int]]:
+
+        if len(sorted_frame_uids) == 0:
+            return []
+
+        if len(sorted_frame_uids) == 1:
+            return [sorted_frame_uids]
+
+        intervals = []
+        interval_start_i = 0
+        for i, frame_uid in enumerate(sorted_frame_uids[1:]):
+            previous_frame_uid = sorted_frame_uids[i]
+
+            if frame_uid - previous_frame_uid > 1:
+                intervals.append(sorted_frame_uids[interval_start_i : i + 1])
+                interval_start_i = i + 1
+
+        intervals.append(sorted_frame_uids[interval_start_i : i + 2])
+        interval_start_i = i + 2
+
+        return intervals
