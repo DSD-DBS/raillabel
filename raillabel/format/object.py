b5a516181fb4b8bd88ff9ed03966efc8cdee620b
Tobias Klockau <tobias.klockau@deutschebahn.com>
2023-08-22 09:20:24 +0200

    feat: Object.object_data_pointers()

--- raillabel/format/object.py
+++ raillabel/format/object.py
@@ -5,6 +5,7 @@ import typing as t
 from dataclasses import dataclass
 from uuid import UUID
 
+from .element_data_pointer import AttributeType, ElementDataPointer
 from .frame_interval import FrameInterval
 
 if t.TYPE_CHECKING:
@@ -29,6 +30,8 @@ class Object:
     uid: str
     name: str
     type: str
+
+    # --- Public Methods --------------
 
     @classmethod
     def fromdict(cls, data_dict: dict, object_uid: str) -> "Object":
@@ -84,5 +87,122 @@ class Object:
 
         return FrameInterval.from_frame_uids(frame_uids_containing_object)
 
+    def object_data_pointers(self, frames: t.Dict[int, "Frame"]) -> t.Dict[str, ElementDataPointer]:
+        """Create object data pointers used in WebLABEL visualization.
+
+        Parameters
+        ----------
+        frames: dict[int, raillabel.format.Frame]
+            The dictionary of frames stored under Scene.frames.
+
+        Returns
+        -------
+        dict[str, ElementDataPointer]
+            ObjectDataPointers dict as required by WebLABEL. Keys are the ObjectDataPointer uids.
+        """
+
+        pointer_ids_per_frame = self._collect_pointer_ids_per_frame(frames)
+        frame_uids_per_pointer_id = self._reverse_frame_pointer_ids(pointer_ids_per_frame)
+        frame_intervals_per_pointer_id = self._convert_to_intervals(frame_uids_per_pointer_id)
+
+        attributes_per_pointer_id = self._collect_attributes_per_pointer_id(frames)
+        attribute_pointers_per_pointer_id = self._convert_to_attribute_pointers(
+            attributes_per_pointer_id
+        )
+
+        return self._create_object_data_pointers(
+            frame_intervals_per_pointer_id, attribute_pointers_per_pointer_id
+        )
+
+    # --- Private Methods -------------
+
     def _is_object_in_frame(self, frame: "Frame") -> bool:
         return UUID(self.uid) in frame.object_data
+
+    def _filtered_annotations(self, frame: "Frame") -> "dict_values":
+        if self.uid not in frame.object_data:
+            return []
+        return frame.object_data[self.uid].annotations.values()
+
+    def _collect_pointer_ids_per_frame(
+        self, frames: t.Dict[int, "Frame"]
+    ) -> t.Dict[int, t.Set[str]]:
+
+        pointer_ids_per_frame = {}
+        for frame in frames.values():
+            pointer_ids_per_frame[frame.uid] = set()
+
+            for annotation in self._filtered_annotations(frame):
+                pointer_ids_per_frame[frame.uid].add(annotation.name)
+
+        return pointer_ids_per_frame
+
+    def _reverse_frame_pointer_ids(
+        self, pointer_ids_per_frame: t.Dict[int, t.Set[str]]
+    ) -> t.Dict[str, t.Set[int]]:
+
+        frame_uids_per_pointer_id = {}
+        for frame_uid, pointer_ids in pointer_ids_per_frame.items():
+            for pointer_id in pointer_ids:
+
+                if pointer_id not in frame_uids_per_pointer_id:
+                    frame_uids_per_pointer_id[pointer_id] = set()
+
+                frame_uids_per_pointer_id[pointer_id].add(frame_uid)
+
+        return frame_uids_per_pointer_id
+
+    def _convert_to_intervals(
+        self, frame_uids_per_pointer_id: t.Dict[str, t.Set[int]]
+    ) -> t.Dict[str, t.List[FrameInterval]]:
+
+        frame_intervals = {}
+        for pointer_id, frame_uids in frame_uids_per_pointer_id.items():
+            frame_intervals[pointer_id] = FrameInterval.from_frame_uids(list(frame_uids))
+
+        return frame_intervals
+
+    def _collect_attributes_per_pointer_id(
+        self, frames: t.Dict[int, "Frame"]
+    ) -> t.Dict[str, t.Dict[str, t.Any]]:
+
+        attributes_per_pointer_id = {}
+        for frame in frames.values():
+            for annotation in self._filtered_annotations(frame):
+                if annotation.name not in attributes_per_pointer_id:
+                    attributes_per_pointer_id[annotation.name] = {}
+
+                attributes_per_pointer_id[annotation.name].update(annotation.attributes)
+
+        return attributes_per_pointer_id
+
+    def _convert_to_attribute_pointers(
+        self, attributes_per_pointer_id: t.Dict[str, t.Dict[str, t.Any]]
+    ) -> t.Dict[str, t.Dict[str, AttributeType]]:
+        for pointer_id, attributes in attributes_per_pointer_id.items():
+            for attribute_name, attribute_value in attributes.items():
+                attributes[attribute_name] = AttributeType.from_value(type(attribute_value))
+
+        return attributes_per_pointer_id
+
+    def _create_object_data_pointers(
+        self,
+        frame_intervals_per_pointer_id: t.Dict[str, t.List[FrameInterval]],
+        attribute_pointers_per_pointer_id: t.Dict[str, t.Dict[str, AttributeType]],
+    ) -> t.Dict[str, ElementDataPointer]:
+
+        object_data_pointers = {}
+        for pointer_id in frame_intervals_per_pointer_id:
+            object_data_pointers[pointer_id] = ElementDataPointer(
+                uid=pointer_id,
+                frame_intervals=frame_intervals_per_pointer_id[pointer_id],
+                attribute_pointers=attribute_pointers_per_pointer_id[pointer_id],
+            )
+
+        return object_data_pointers
+
+    # --- Special Methods -------------
+
+    def __hash__(self) -> int:
+        """Return hash."""
+        return self.uid.__hash__()
