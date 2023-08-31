0e69f87a673f20de03e792753501b322199ba11a
Tobias Klockau <tobias.klockau@deutschebahn.com>
2023-08-31 12:57:01 +0200

    test: separate SensorReference tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -65,6 +65,10 @@ from test_raillabel.format.test_sensor import (
     sensor_radar,
     sensor_radar_dict,
 )
+from test_raillabel.format.test_sensor_reference import (
+    sensor_reference_camera,
+    sensor_reference_camera_dict,
+)
 from test_raillabel.format.test_size2d import size2d, size2d_dict
 from test_raillabel.format.test_size3d import size3d, size3d_dict
 from test_raillabel.format.test_transform import transform, transform_dict
