630b99740f8d19d274ea4f23cbda828dbd5e3156
Tobias Klockau <tobias.klockau@deutschebahn.com>
2023-09-04 15:26:14 +0200

    test: detached uai SensorReference tests from .json file

--- tests/test_raillabel/format/understand_ai/__init__.py
+++ tests/test_raillabel/format/understand_ai/__init__.py
@@ -2,3 +2,11 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from .test_uai_attributes import attributes_raillabel, attributes_uai, attributes_uai_dict
+from .test_uai_sensor_reference import (
+    sensor_camera_raillabel_dict,
+    sensor_camera_uai,
+    sensor_camera_uai_dict,
+    sensor_lidar_raillabel_dict,
+    sensor_lidar_uai,
+    sensor_lidar_uai_dict,
+)
--- tests/test_raillabel/format/understand_ai/test_uai_sensor_reference.py
+++ tests/test_raillabel/format/understand_ai/test_uai_sensor_reference.py
@@ -1,44 +1,107 @@
 # Copyright DB Netz AG and contributors
 # SPDX-License-Identifier: Apache-2.0
 
-import os
 import sys
 from decimal import Decimal
 from pathlib import Path
 
 import pytest
 
-sys.path.insert(1, str(Path(__file__).parent.parent.parent.parent))
+sys.path.insert(1, str(Path(__file__).parent.parent.parent.parent.parent))
 
 import raillabel.format.understand_ai as uai_format
 
+# == Fixtures =========================
 
-def test_fromdict(json_data):
-    input_data = json_data["_understand_ai_t4_format/sensor_reference_camera"]
-    sensor_reference = uai_format.SensorReference.fromdict(input_data)
+@pytest.fixture
+def sensor_camera_uai_dict() -> dict:
+    return {
+        "type": "ir_middle",
+        "uri": "A0001781_image/000_1632321843.100464760.png",
+        "timestamp": "1632321843.100464760"
+    }
 
-    assert sensor_reference.type == input_data["type"]
-    assert sensor_reference.uri == input_data["uri"]
-    assert sensor_reference.timestamp == Decimal(input_data["timestamp"])
-
-def test_to_raillabel(json_data):
-    sensor_reference = uai_format.SensorReference.fromdict(
-        json_data["_understand_ai_t4_format/sensor_reference_camera"]
+@pytest.fixture
+def sensor_camera_uai() -> dict:
+    return uai_format.SensorReference(
+        type="ir_middle",
+        uri="A0001781_image/000_1632321843.100464760.png",
+        timestamp=Decimal("1632321843.100464760"),
     )
-    sensor_id, output_data = sensor_reference.to_raillabel()
 
-    assert sensor_id == sensor_reference.type
-    assert output_data == {
+@pytest.fixture
+def sensor_camera_raillabel_dict() -> dict:
+    return {
         "stream_properties": {
             "sync": {
-                "timestamp": str(sensor_reference.timestamp)
+                "timestamp": "1632321843.100464760"
             }
         },
-        "uri": sensor_reference.uri
+        "uri": "A0001781_image/000_1632321843.100464760.png"
     }
 
 
-# Executes the test if the file is called
+@pytest.fixture
+def sensor_lidar_uai_dict() -> dict:
+    return {
+        "type": "LIDAR",
+        "uri": "lidar_merged/000_1632321880.132833000.pcd",
+        "timestamp": "1632321880.132833000"
+    }
+
+@pytest.fixture
+def sensor_lidar_uai() -> dict:
+    return uai_format.SensorReference(
+        type="LIDAR",
+        uri="lidar_merged/000_1632321880.132833000.pcd",
+        timestamp=Decimal("1632321880.132833000"),
+    )
+
+@pytest.fixture
+def sensor_lidar_raillabel_dict() -> dict:
+    return {
+        "stream_properties": {
+            "sync": {
+                "timestamp": "1632321880.132833000"
+            }
+        },
+        "uri": "lidar_merged/000_1632321880.132833000.pcd"
+    }
+
+# == Tests ============================
+
+def test_fromdict():
+    sensor_reference = uai_format.SensorReference.fromdict(
+        {
+            "type": "ir_middle",
+            "uri": "A0001781_image/000_1632321843.100464760.png",
+            "timestamp": "1632321843.100464760"
+        }
+    )
+
+    assert sensor_reference.type == "ir_middle"
+    assert sensor_reference.uri == "A0001781_image/000_1632321843.100464760.png"
+    assert sensor_reference.timestamp == Decimal("1632321843.100464760")
+
+
+def test_to_raillabel():
+    sensor_reference = uai_format.SensorReference(
+        type="ir_middle",
+        uri="A0001781_image/000_1632321843.100464760.png",
+        timestamp=Decimal("1632321843.100464760"),
+    )
+
+    assert sensor_reference.to_raillabel()[0] == "ir_middle"
+    assert sensor_reference.to_raillabel()[1] == {
+        "stream_properties": {
+            "sync": {
+                "timestamp": "1632321843.100464760"
+            }
+        },
+        "uri": "A0001781_image/000_1632321843.100464760.png"
+    }
+
 if __name__ == "__main__":
+    import os
     os.system("clear")
     pytest.main([__file__, "--disable-pytest-warnings", "--cache-clear"])
