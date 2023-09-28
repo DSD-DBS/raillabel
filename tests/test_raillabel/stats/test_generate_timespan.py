94ade2fdd3a019be620295607db9802487fa74a7
Cansu Baskaya <cansu.baskaya@deutschebahn.com>
2023-09-28 14:25:14 +0200

    feat: create generate timespan

---
+++ tests/test_raillabel/stats/test_generate_timespan.py
@@ -0,0 +1,71 @@
+# Copyright DB Netz AG and contributors
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+import sys
+from pathlib import Path
+
+import pytest
+
+sys.path.insert(1, str(Path(__file__).parent.parent.parent.parent))
+
+import raillabel
+
+# == Fixtures ============================
+
+@pytest.fixture
+def metadata() -> raillabel.format.Metadata:
+    return raillabel.format.Metadata(schema_version="1.0.0")
+
+# == Tests ============================
+
+def test_simple_timespan(metadata):
+    scene = raillabel.Scene(
+        metadata=metadata,
+        frames={
+            0: raillabel.format.Frame(
+                uid=0,
+                timestamp=100
+            ),
+            1: raillabel.format.Frame(
+                uid=1,
+                timestamp=105
+            ),
+            2: raillabel.format.Frame(
+                uid=0,
+                timestamp=110
+            ),
+        }
+    )
+
+    assert raillabel.stats.generate_timespan(scene) == (100, 110)
+
+def test_unordered_timspan(metadata):
+    scene = raillabel.Scene(
+        metadata=metadata,
+        frames={
+            0: raillabel.format.Frame(
+                uid=0,
+                timestamp=110
+            ),
+            1: raillabel.format.Frame(
+                uid=1,
+                timestamp=100
+            ),
+        }
+    )
+
+    assert raillabel.stats.generate_timespan(scene) == (100, 110)
+
+def test_empty_timespan(metadata):
+    scene = raillabel.Scene(
+        metadata=metadata,
+        frames={}
+    )
+
+    assert raillabel.stats.generate_timespan(scene) == (None, None)
+
+
+if __name__ == "__main__":
+    os.system("clear")
+    pytest.main([__file__, "--disable-pytest-warnings", "--cache-clear", "-v"])
