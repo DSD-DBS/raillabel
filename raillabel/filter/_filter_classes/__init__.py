a13188aa03adb981dac708858db53b949e37147f
Tobias Klockau <tobias.klockau@deutschebahn.com>
2024-10-21 16:25:48 +0200

    chore: update all pre-commit dependencies

--- raillabel/filter/_filter_classes/__init__.py
+++ raillabel/filter/_filter_classes/__init__.py
@@ -9,7 +9,7 @@ from pkgutil import iter_modules
 
 # iterate through the modules in the current package
 package_dir = str(Path(__file__).resolve().parent)
-for (_, module_name, _) in iter_modules([package_dir]):
+for _, module_name, _ in iter_modules([package_dir]):
 
     # import the module and iterate through its attributes
     module = import_module(f"{__name__}.{module_name}")
