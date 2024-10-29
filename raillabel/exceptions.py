bb0171639bdaf06beeb94c05d9f7d0eb444018af
unexcellent <>
2024-10-29 15:09:23 +0100

    refactor: remove unused exceptions

--- raillabel/exceptions.py
+++ raillabel/exceptions.py
@@ -2,27 +2,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
 
-class UnsupportedFormatError(Exception):
-    """Raised when a loaded annotation file is not in a supported format."""
-
-    __module__ = "raillabel"
-
-
-class SchemaError(Exception):
-    """Raised when the data does not validate against a given schema."""
-
-    __module__ = "raillabel"
-
-
-class AmbiguousSchemaNameError(Exception):
-    """Raised when a schema key applies to more than one schema files in.
-
-    /schemas.
-    """
-
-    __module__ = "raillabel"
-
-
 class MissingStreamError(Exception):
     """Raised when a coordinate system has no corresponding stream."""
 
