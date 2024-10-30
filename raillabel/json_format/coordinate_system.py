4a0979fe67dd5971e2ea4944267d6df021f8a443
unexcellent <>
2024-10-30 09:43:40 +0100

    feat: add JSONCoordinateSystem

---
+++ raillabel/json_format/coordinate_system.py
@@ -0,0 +1,18 @@
+# Copyright DB InfraGO AG and contributors
+# SPDX-License-Identifier: Apache-2.0
+
+from __future__ import annotations
+
+from typing import Literal
+
+from pydantic import BaseModel
+from transform_data import JSONTransformData
+
+
+class JSONCoordinateSystem(BaseModel):
+    """A 3D reference frame."""
+
+    children: list[str]
+    parent: Literal["base", ""]
+    pose_wrt_parent: JSONTransformData
+    type: Literal["sensor", "local"]
