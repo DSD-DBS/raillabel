b00df0c0e8666dfd673df1b027db066c255bad95
unexcellent <>
2024-11-13 10:01:26 +0100

    feat: implement Point2d.to_json()

--- raillabel/format/point2d.py
+++ raillabel/format/point2d.py
@@ -20,3 +20,7 @@ class Point2d:
     def from_json(cls, json: tuple[float, float]) -> Point2d:
         """Construct an instant of this class from RailLabel JSON data."""
         return Point2d(x=json[0], y=json[1])
+
+    def to_json(self) -> tuple[float, float]:
+        """Export this object into the RailLabel JSON format."""
+        return (self.x, self.y)
