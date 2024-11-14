737a97b881fc43f1e5ba0a47ae7d9fd42b82fa5a
unexcellent <>
2024-11-14 10:08:53 +0100

    feat: implement raillabel.save()

--- raillabel/save/save.py
+++ raillabel/save/save.py
@@ -8,19 +8,12 @@ from pathlib import Path
 from raillabel.format import Scene
 
 
-def save(_scene: Scene, _path: Path | str, _prettify_json: bool = False) -> None:
-    """Save a raillabel.Scene in a JSON file.
+def save(scene: Scene, path: Path | str, prettify_json: bool = False) -> None:
+    """Save a raillabel.Scene to a JSON file."""
+    if prettify_json:
+        json_data = scene.to_json().model_dump_json(indent=4)
+    else:
+        json_data = scene.to_json().model_dump_json()
 
-    Parameters
-    ----------
-    scene: raillabel.Scene
-        Scene, which should be saved.
-    path: str
-        Path to the file location, that should be used for saving.
-    save_path: str
-        Path to the JSON file.
-    prettify_json: bool, optional
-        If true, the JSON is saved with linebreaks and indents. This increases readibility but
-        also the file size. Default is False.
-
-    """
+    with Path(path).open("w") as scene_file:
+        scene_file.write(json_data)
