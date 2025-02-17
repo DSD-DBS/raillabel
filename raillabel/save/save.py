b98eb66bad1e61761fd702f52ec045ea466f3815
unexcellent <>
2025-02-17 13:19:12 +0100

    docs: add examples to docstrings

--- raillabel/save/save.py
+++ raillabel/save/save.py
@@ -9,7 +9,20 @@ from raillabel.format import Scene
 
 
 def save(scene: Scene, path: Path | str, prettify_json: bool = False) -> None:
-    """Save a raillabel.Scene to a JSON file."""
+    """Save a raillabel.Scene to a JSON file.
+
+    Example:
+    ```
+    import raillabel
+    scene = raillabel.load("path/to/scene.json")
+
+    # change something about the scene
+
+    raillabel.save(scene, "path/to/new_scene.json")
+    # or to get a readable (but much larger) file
+    raillabel.save(scene, "path/to/new_scene.json", prettify_json=True)
+    ```
+    """
     if prettify_json:
         json_data = scene.to_json().model_dump_json(exclude_none=True, indent=4)
     else:
